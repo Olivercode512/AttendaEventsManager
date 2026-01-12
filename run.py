b71a7d10d46fecb98941f82f45831fe504524d34
Olivercode512 <pauloliveror@gmail.com>
2026-01-12 01:39:43 +0100

    Añado mensaje de prueba al inicio de run.py

--- run.py
+++ run.py
@@ -1,2 +1,5 @@
 import os
+st.title("¡APP INICIADA! Prueba de debug")
+st.write("Si ves esto, la app arrancó correctamente. El problema está después.")
+st.balloons()
 os.system("streamlit run app/main.py --server.port 8502")
